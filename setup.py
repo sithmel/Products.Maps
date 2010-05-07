b67853a9f7003add4ac759894ae2da05fe86c0c7
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-05-07 12:33:51 +0000

    Prepare Products.Maps 2.0.3.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
 version = open(os.path.join('Products', 'Maps', 'version.txt')).read().strip()
 readme = open("README.txt").read()
