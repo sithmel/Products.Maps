699e76066fd081ffae5feaa1534bb0dbbcb8c645
Luca Fabbri <luca@keul.it>
2013-01-06 12:53:28 +0100

    fixed classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from os.path import join
 
 version = '3.0.0dev'
 
-
 setup(name='Products.Maps',
       version=version,
       description="A simple, easy to use Plone integration with Google Maps "
@@ -12,13 +11,16 @@ setup(name='Products.Maps',
                        open(join('docs','HISTORY.txt')).read(),
       classifiers=[
         "Framework :: Zope2",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
       ],
       keywords='Google Maps Zope Plone',
       author='Florian Schulze',
       author_email='fschulze@jarn.com',
-      url='http://pypi.python.org/pypi/Products.Maps',
+      url='http://plone.org/products/maps',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
