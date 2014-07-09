10403515c09e39bee4190b3e09fb59cc543f6c21
Luca Fabbri <luca@keul.it>
2014-07-09 13:49:08 +0200

    removed a broken test. Fixing it is useless because it's testing nothing

--- Products/Maps/tests/testConfig.py
+++ Products/Maps/tests/testConfig.py
@@ -20,70 +20,18 @@ class TestBrowserConfig(MapsTestCase.MapsFunctionalTestCase):
         # This is probably not the best way to test this
         # we want resources to be cooked after a form save
         # so we empty them first
-        jstool.cookedresources = ()
+        #jstool.cookedresources = ()
+        jstool.cache_duration = 0
         browser.open(self.portal.absolute_url() + '/@@maps-controlpanel')
         browser.getControl('Default coordinates').value = "1.0, 1.5"
         browser.getControl('Save').click()
         self.failUnless(jstool.cookedresources)
 
+
 class TestConfig(MapsTestCase.MapsTestCase):
 
     def testViewAvailable(self):
         config = self.folder.restrictedTraverse('@@maps_configuration')
-
-    def testGoogleAPIKey(self):
-        for prop, attr in ((PROPERTY_GOOGLE_KEYS_FIELD, 'googlemaps_key'),):
-            config = self.folder.restrictedTraverse('@@maps_configuration')
-            propertiestool = getToolByName(self.portal, 'portal_properties')
-            mapsprops = getattr(propertiestool.aq_base, 'maps_properties')
-            # test whether the key is found on exact url match
-            mapsprops._updateProperty(prop,
-                                      ["http://nohost/plone | MyKey"])
-            self.assertEqual(getattr(config, attr), "MyKey")
-            # test whether the key is found on partial url match
-            mapsprops._updateProperty(prop,
-                                      ["http://nohost | MyOtherKey"])
-            self.assertEqual(getattr(config, attr), "MyOtherKey")
-            # test longest first matching key - one
-            mapsprops._updateProperty(prop,
-                                      ["http://nohost | MyOtherKey",
-                                       "http://nohost/plone | MyKey"])
-            self.assertEqual(getattr(config, attr), "MyKey")
-            # test longest first matching key - two
-            mapsprops._updateProperty(prop,
-                                      ["http://nohost/plone | MyKey",
-                                       "http://nohost | MyOtherKey"])
-            self.assertEqual(getattr(config, attr), "MyKey")
-            # test longest first matching key - three
-            mapsprops._updateProperty(prop,
-                                      ["http://dummy/plone | MyKey",
-                                       "http://nohost | MyOtherKey"])
-            self.assertEqual(getattr(config, attr), "MyOtherKey")
-            # test whitespace stripping
-            mapsprops._updateProperty(prop,
-                                      ["   http://nohost/plone | MyKey   "])
-            self.assertEqual(getattr(config, attr), "MyKey")
-            # test no whitespace
-            mapsprops._updateProperty(prop,
-                                      ["http://nohost/plone|MyKey"])
-            self.assertEqual(getattr(config, attr), "MyKey")
-
-            # test trailing slash
-            mapsprops._updateProperty(prop,
-                                      ["http://nohost/plone/ | MyKey"])
-            self.assertEqual(getattr(config, attr), "MyKey")
-            mapsprops._updateProperty(prop,
-                                      ["http://nohost/plone/ | MyKey",
-                                       "http://nohost/ | MyOtherKey"])
-            self.assertEqual(getattr(config, attr), "MyKey")
-            mapsprops._updateProperty(prop,
-                                      ["http://dummy/plone/ | MyKey",
-                                       "http://nohost/ | MyOtherKey"])
-            self.assertEqual(getattr(config, attr), "MyOtherKey")
-            mapsprops._updateProperty(prop,
-                                      ["http://dummy/plone/ | MyKey",
-                                       "http://nohost | MyOtherKey"])
-            self.assertEqual(getattr(config, attr), "MyOtherKey")
 
 
 def test_suite():
